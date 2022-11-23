a42c191dfe4af253f178d11c6dc0e36e661675fa
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2022-11-23 15:50:13 +0000

    classes and components created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>user info goes here</header>
+      <section>sidebar with multiple users to chat with</section>
+      <section>main - chat history goes here</section>
+      <footer>this is where the message input goes</footer>
     </div>
   );
 }
