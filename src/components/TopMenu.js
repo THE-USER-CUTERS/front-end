b9dae460feabb045a614515460c92ac9189be9c8
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2022-11-23 17:09:11 +0000

    added more chats and scroll

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -4,12 +4,17 @@ export default function TopMenu(){
     return(
         <div>
             <div className="top-menu">
-                <i className='fa fa-user-circle icon-menu'></i>
-                <i className="fa fa-comment icon-menu"></i>
-                <i className="fa fa-ellipsis-v icon-menu"></i>
+                <i className='fa fa-user-circle icon-menu pointer'></i>
+                <i className="fa fa-comment icon-menu pointer"></i>
+                <i className="fa fa-ellipsis-v icon-menu pointer"></i>
             </div>
             <input type="search" className="custon-search-input" placeholder="Search messages..."/>
-                <Friend name={"Lucas"} time={"12:40"} text={"ksjdhdshdsgdhsdggfsdf;sfkjelkfjekhroeshfkljsdklfjdlkfjhldsha"}/>
+                <div className="scroll">
+                <Friend name={"Lucas"} time={"12:40"} text={"Have you seen my message?"}/>
+                <Friend name={"Sofia"} time={"1:35"} text={"Hi how are you today? Looking forward to see you later"}/>
+                <Friend name={"Bank Account"} time={"12:40"} text={"Verify Your Bank Account"}/>
+                <Friend name={"Mum"} time={"10:00"} text={"Are you coming to have lunch? You dad is coming!"}/>
+                </div>
             </div>
     )
 }
