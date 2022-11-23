12cce1b524ced6b050d4832aa5607a48f47a5c35
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2022-11-23 17:16:48 +0000

    filter functionality working

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import Friend from "./Friend";
 
+const friends = [
+    {name: 'Lucas', time: '12:30', text: "Have you seen my message?"},
+    {name: 'Sofia', time: '1:35', text: "Hi how are you today? Looking forward to see you later"},
+    {name: 'Bank Account', time: '16:50', text: "Have you seen my message?"},
+    {name: 'Mum', time: '10:00', text: "Are you coming to have lunch? You dad is coming!"}
+
+]
+
 export default function TopMenu(){
+
+    const [showFriends, setShowFriends] = useState(friends);
+
+    function filter(filterText){
+        console.log(filterText);
+        if(filterText !== '' && filterText){
+            let provFriends = friends.filter(x => x.name.includes(filterText) || x.text.includes(friends));
+            setShowFriends(provFriends);
+        } else {
+            setShowFriends(friends);
+        }
+
+    }
+
+
     return(
         <div>
             <div className="top-menu">
@@ -8,12 +32,9 @@ export default function TopMenu(){
                 <i className="fa fa-comment icon-menu pointer"></i>
                 <i className="fa fa-ellipsis-v icon-menu pointer"></i>
             </div>
-            <input type="search" className="custon-search-input" placeholder="Search messages..."/>
+            <input type="search" onChange={(e) => filter(e.target.value)} className="custon-search-input" placeholder="Search messages..."/>
                 <div className="scroll">
-                <Friend name={"Lucas"} time={"12:40"} text={"Have you seen my message?"}/>
-                <Friend name={"Sofia"} time={"1:35"} text={"Hi how are you today? Looking forward to see you later"}/>
-                <Friend name={"Bank Account"} time={"12:40"} text={"Verify Your Bank Account"}/>
-                <Friend name={"Mum"} time={"10:00"} text={"Are you coming to have lunch? You dad is coming!"}/>
+                    {showFriends.map((friend, index) =><Friend key={index} name={friend.name} time={friend.time} text={friend.text}/>)}
                 </div>
             </div>
     )
